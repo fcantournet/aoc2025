50b8993084db593867f058ad64895539ba4ef8c3
felix <Flix Cantournet>
2025-12-06 12:24:55 +0100

    day05 clean up

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -7,10 +7,9 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut db = parse_input(input);
+    let db = parse_input(input);
 
-    let merged = db.merge_ranges();
-    dbg!(&merged);
+    let merged = db.merged_ranges();
     Some(merged.iter().map(|range| range.end - range.start + 1).sum())
 }
 
@@ -71,20 +70,19 @@ impl DB {
         fresh
     }
 
-    fn merge_ranges(&mut self) -> Vec<Range> {
-        dbg!(&self.ranges);
-        self.ranges.sort();
-        dbg!(&self.ranges);
-        let mut current = self.ranges[0];
+    fn merged_ranges(&self) -> Vec<Range> {
+        let mut sorted = self.ranges.clone();
+        sorted.sort();
+        let mut current = sorted[0];
         let mut merged = Vec::new();
-        for next in self.ranges[1..].iter().copied() {
+        for next in sorted[1..].iter().copied() {
             if current.overlaps(&next) {
                 // let's merge them
                 current.end = current.end.max(next.end);
-                dbg!(current, next);
+                // dbg!(current, next);
             } else {
                 merged.push(current);
-                dbg!(&merged);
+                // dbg!(&merged);
                 current = next;
             }
         }
