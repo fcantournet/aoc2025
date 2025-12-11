0ac4de857ca1046299d0340d35ea3b0682fd0aa0
felix <Flix Cantournet>
2025-12-11 15:25:34 +0100

    day09 raycasting ?

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,4 +1,4 @@
-use glam::IVec2;
+use glam::I64Vec2;
 
 advent_of_code::solution!(9);
 
@@ -19,50 +19,136 @@ pub fn part_two(input: &str) -> Option<u64> {
 
     display(&points, 12);
 
-    let mut perimeter: Vec<(IVec2, IVec2)> = Vec::new();
+    let mut perimeter: Vec<(I64Vec2, I64Vec2)> = Vec::new();
     for (p1, p2) in points.iter().zip(points.iter().cycle().skip(1)) {
-        perimeter.push((*p1, p2-p1));
+        perimeter.push((*p1, *p2));
     }
+
+    let verts: Vec<_> = perimeter
+        .iter()
+        .copied()
+        .filter(|(p1, p2)| I64Vec2::X.dot(p2 - p1) == 0)
+        .collect();
+
+    let horizontals: Vec<_> = perimeter
+        .iter()
+        .copied()
+        .filter(|(p1, p2)| I64Vec2::Y.dot(p2 - p1) == 0)
+        .collect();
 
     let mut rects = Vec::new();
     for (i, p1) in points.iter().enumerate() {
-        for p2 in points.iter().skip(i+1) {
-            if rect_inside(&perimeter, p1, p2) {
-                rects.push(makes_rect(p1, p2));
+        for p2 in points.iter().skip(i + 1) {
+            rects.push(((p1, p2), makes_rect(p1, p2)));
+        }
+    }
+    rects.sort_by_key(|(_, size)| *size);
+
+    let largest = rects
+        .iter()
+        .rev()
+        .find(|((p1, p2), _size)| rect_inside(&perimeter, &verts, &horizontals, p1, p2));
+
+    dbg!(largest);
+    Some(largest.unwrap().1 as u64)
+}
+
+fn rect_inside(
+    perimeter: &[(I64Vec2, I64Vec2)],
+    verts: &[(I64Vec2, I64Vec2)],
+    horizontals: &[(I64Vec2, I64Vec2)],
+    p1: &I64Vec2,
+    p2: &I64Vec2,
+) -> bool {
+    println!("does {} - {} fit ?", p1, p2);
+    let p3 = I64Vec2 { x: p1.x, y: p2.y };
+    let p4 = I64Vec2 { x: p2.x, y: p1.y };
+    [&p3, &p4].iter().all(|p| {
+        // fast check if we are ON the perimeter we are inside.
+        if on_perimeter(perimeter, p) {
+            true
+        } else {
+            inside_by_ray_casting(verts, horizontals, p)
+        }
+    })
+}
+
+// Check if points are ON the perimeter
+// Turns out the 2 other corners of the rectangle can be inside the perimeter not on it.
+fn on_perimeter(perimeter: &[(I64Vec2, I64Vec2)], p1: &I64Vec2) -> bool {
+    for (start, end) in perimeter {
+        let maxx = start.x.max(end.x);
+        let minx = start.x.min(end.x);
+        let maxy = start.y.max(end.y);
+        let miny = start.y.min(end.y);
+        if p1.x >= minx && p1.x <= maxx && p1.y >= miny && p1.y <= maxy {
+            return true;
+        }
+    }
+    return false;
+}
+
+// I think this is technically ray casting not tracing ?
+// Anyway from a point p we cast 4 rays, 1 in each cardinal direction
+// and we check if we intersect with the perimeter, if we do in all directions we're inside.
+// We can greatly reduce the search space and have a simple boundary condition to end the ray casting
+// by only trying to intersect with segments of the perimeter which are orthogonal to us and are in the direction
+// we are casting.
+fn inside_by_ray_casting(
+    verts: &[(I64Vec2, I64Vec2)],
+    horizontals: &[(I64Vec2, I64Vec2)],
+    p: &I64Vec2,
+) -> bool {
+    // vertical checks
+    let vert_intersect = [I64Vec2::X, I64Vec2::NEG_X].iter().all(|dir| {
+        for (p1, p2) in verts {
+            // p1.x == p2.x
+            if (p.x - p1.x) * dir.x < 0 {
+                // if p * dir intersects [p1,p2] then p.y is between p1 and p2
+                // so one diff is positive (or 0) and the other negative (or 0), so we can multiply them and check <= 0.
+                if (p.y - p1.y) * (p.y - p2.y) <= 0 {
+                    return true;
+                }
             }
         }
+        false
+    });
+    if !vert_intersect {
+        return false;
     }
-    Some(rects.into_iter().max().unwrap() as u64)
-}
 
-fn rect_inside(perimeter: &[(IVec2, IVec2)], p1: &IVec2, p2: &IVec2) -> bool {
-    println!("does {} - {} fit ?", p1, p2);
-    let p3 = IVec2{x: p1.x, y: p2.y};
-    let p4 = IVec2{x: p2.x, y: p1.y};
-    [p1, p2, &p3, &p4].iter().all(|p| in_perimeter(perimeter, p))
-}
-
-fn in_perimeter(perimeter: &[(IVec2, IVec2)], p1: &IVec2) -> bool {
-    for (start, dir) in perimeter {
-        if dir.perp_dot(p1-start) >= 0 {
-            println!("  nope because: {} is not on the right side of {} -> {}", p1, start, dir);
-            return false
+    // horizontal checks
+    let horizontal_intersect = [I64Vec2::Y, I64Vec2::NEG_Y].iter().all(|dir| {
+        for (p1, p2) in horizontals {
+            // p1.y == p2.y
+            if (p.y - p1.y) * dir.y < 0 {
+                // if p * dir intersects [p1,p2] then p.y is between p1 and p2
+                // so one diff is positive (or 0) and the other negative (or 0), so we can multiply them and check <= 0.
+                if (p.x - p1.x) * (p.x - p2.x) <= 0 {
+                    return true;
+                }
+            }
         }
-    }
-    return true
+        false
+    });
+
+    vert_intersect && horizontal_intersect
 }
 
-fn parse_input(input: &str) -> Vec<IVec2> {
-    input.lines().map(|s| {
-        let (a, b) = s.split_once(",").unwrap();
-        IVec2{
-            x: a.parse().unwrap(),
-            y: b.parse().unwrap(),
-        }
-    }).collect()
+fn parse_input(input: &str) -> Vec<I64Vec2> {
+    input
+        .lines()
+        .map(|s| {
+            let (a, b) = s.split_once(",").unwrap();
+            I64Vec2 {
+                x: a.parse().unwrap(),
+                y: b.parse().unwrap(),
+            }
+        })
+        .collect()
 }
 
-fn display(points: &[IVec2], size: usize) {
+fn display(points: &[I64Vec2], size: usize) {
     let mut grid = vec![vec!['.'; size]; size];
     for point in points {
         grid[point.y as usize][point.x as usize] = '#';
@@ -72,14 +158,13 @@ fn display(points: &[IVec2], size: usize) {
     }
 }
 
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
     x: isize,
     y: isize,
 }
 
-fn makes_rect(p1: &IVec2, p2: &IVec2) -> usize {
+fn makes_rect(p1: &I64Vec2, p2: &I64Vec2) -> usize {
     (1 + (p1.x - p2.x).abs() as usize) * (1 + (p1.y - p2.y).abs() as usize)
 }
 
@@ -96,6 +181,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24));
     }
 }
